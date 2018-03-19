3215c0273f0497c7d63731760f3dd4cba7cb390d
Murderlon <hello@murderlon.me>
2018-03-19 22:41:14 +0100

    handle delete request in mysql

--- server/routes/animal.js
+++ server/routes/animal.js
@@ -4,18 +4,16 @@ const head = require('lodash.head');
 
 const pool = require('../mysql');
 const helpers = require('../helpers');
-const db = require('../../db');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  // if (db.removed(id)) {
-  //   return res.status(410).send(statusCode[410]);
-  // }
   try {
     const done = (err, result) => {
-      if (err) next(err);
+      if (err || result.length === 0) {
+        next();
+      }
       const data = {
         data: head(result),
         ...helpers
@@ -36,19 +34,16 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.delete('/:id', (req, res, next) => {
-  if (db.removed(req.params.id)) {
-    return res.status(410).send(statusCode[410]);
-  }
-  try {
-    if (db.has(req.params.id)) {
-      db.remove(req.params.id);
-      res.status(204).send(statusCode[204]);
-    } else {
-      next();
-    }
-  } catch (err) {
-    res.status(400).send(statusCode[400]);
-  }
+  const id = req.params.id;
+  const done = (err, { affectedRows }) =>
+    err
+      ? next(err)
+      : affectedRows === 0 ? next() : res.status(204).send(statusCode[204]);
+  pool.getConnection((err, connection) => {
+    connection.query('DELETE FROM animal WHERE id = ?', id, done);
+    connection.release();
+    if (err) throw err;
+  });
 });
 
 module.exports = router;
